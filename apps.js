b19c50445c9d0589191f3e98034b2756ab1d64a4
Dana Sugu <dana.sugu@gmail.com>
2021-09-28 20:04:20 +0300

    Update apps.js

--- apps.js
+++ apps.js
@@ -1,4 +1,53 @@
 document.addEventListener('DOMContentLoaded', () => {
   //card options
-
-})
+  const cardArray = [
+    {
+      name: 'fries',
+      image: 'images/fries.png',
+    },
+    {
+      name: 'fries',
+      image: 'images/fries.png',
+    },
+    {
+      name: 'cheeseburger',
+      image: 'images/cheeseburger.png',
+    },
+    {
+      name: 'cheeseburger',
+      image: 'images/cheeseburger.png',
+    },
+    {
+      name: 'hotdog',
+      image: 'images/hotdog.png',
+    },
+    {
+      name: 'hotdog',
+      image: 'images/hotdog.png',
+    },
+    {
+      name: 'ice-cream',
+      image: 'images/ice-cream.png',
+    },
+    {
+      name: 'ice-cream',
+      image: 'images/ice-cream.png',
+    },
+    {
+      name: 'milkshake',
+      image: 'images/milkshake.png',
+    },
+    {
+      name: 'milkshake',
+      image: 'images/milkshake.png',
+    },
+    {
+      name: 'pizza',
+      image: 'images/pizza.png',
+    },
+    {
+      name: 'pizza',
+      image: 'images/pizza.png',
+    },
+  ];
+});
